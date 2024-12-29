53ec3df428006a1f02d4829116efb3b3ec9b19af
igor.smolka <ihar_smolka@senla.eu>
2024-12-29 23:44:25 +0300

    questionary service

--- igodating-geodata-service/src/main/java/com/igodating/geodata/config/ApplicationConfig.java
+++ igodating-geodata-service/src/main/java/com/igodating/geodata/config/ApplicationConfig.java
@@ -1,8 +1,10 @@
 package com.igodating.geodata.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 public class ApplicationConfig {
 
 

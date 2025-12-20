4f38d0394325c2caf9177404e2869ceb0e3c9a28
igor.smolka <ihar_smolka@senla.eu>
2025-12-20 13:51:15 +0300

    refactor for ResponseWrapper

---
+++ igodating-geodata-service/src/main/java/com/igodating/geodata/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.igodating.geodata.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("*") // Allow all methods (GET, POST, PUT, DELETE, etc.)
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(false) // Set to true if you need to support cookies/auth
+                .maxAge(3600);
+    }
+}
